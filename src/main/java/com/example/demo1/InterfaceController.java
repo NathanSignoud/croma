c75db266bfa15dd3d94e1f1b8b6ec49fbbc2a00d
Nathan Signoud <signoudnat@cy-tech.fr>
2024-05-26 05:11:28 +0200

    Toutes les fonctions de dessin qui marchent (a peu près)

--- src/main/java/com/example/demo1/InterfaceController.java
+++ src/main/java/com/example/demo1/InterfaceController.java
@@ -13,7 +13,6 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.Slider;
 import javafx.scene.image.WritableImage;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
@@ -35,31 +34,30 @@ public class InterfaceController {
 
     @FXML
     private Pane drawingPane;
-
     @FXML
     private Button stepButton;
     @FXML
-
     private List<String> drawingInstructions;
-    private int currentInstructionIndex = 0;
-    private Timeline timeline;
-    private GraphicsContext gc;
-    private boolean hasDrawn = false;
-    private Scene scene;
     @FXML
     private Button loadFileButton;
     @FXML
     private Button slowSpeedButton;
-
     @FXML
     private Button mediumSpeedButton;
-
     @FXML
     private Button fastSpeedButton;
-
     @FXML
     private ComboBox<String> speedComboBox;
 
+    private int currentInstructionIndex = 0;
+
+    private Timeline timeline;
+
+    private GraphicsContext gc;
+
+    private boolean hasDrawn = false;
+
+    private Scene scene;
 
     public class Cursor {
 
@@ -313,6 +311,110 @@ public class InterfaceController {
         return 1;
     }
 
+    public int COLORHEX(String hex){
+        Color color = Color.web(hex);  // Take the second element as a hexadecimal code or colour name
+        gc.setStroke(color);
+        return 1;
+    }
+
+    public int COLORRGB(double red, double green, double blue){
+
+        Color color = Color.color(red, green, blue);;  // Take the second element as a hexadecimal code or colour name
+        gc.setStroke(color);
+        return 1;
+    }
+
+    public int MOV(Cursor c, double x, double y, int percent){
+        if(percent != 0 && percent != 1){
+            System.out.println("You must put a number between 0 and 1");
+            return 0;
+        }
+        if(percent == 1){
+             x = drawingPane.getPrefWidth() * x;
+             y = drawingPane.getPrefHeight() * y;
+        }
+
+        x = c.x + x;
+        y = c.y + y;
+
+        if( x > drawingPane.getWidth() || x < 0){
+            System.out.println("You are out of borders, you cannot move outside of " + drawingPane.getWidth());
+            return 0;
+        }
+        if( y > drawingPane.getHeight()  || y < 0){
+            System.out.println("You are out of borders, you cannot move outside of" + drawingPane.getHeight());
+            return 0;
+        }
+        c.x = x;                               // New coordinates
+        c.y = y;
+        gc.moveTo(x, y);
+        return 1;
+    }
+
+    public int POS(Cursor c, double x, double y, int percent){
+        if(percent != 0 && percent != 1){
+            System.out.println("You must put a number between 0 and 1");
+            return 0;
+        }
+        if(percent == 1){
+            x = drawingPane.getPrefWidth() * x;
+            y = drawingPane.getPrefHeight() * y;
+        }
+        if( x > drawingPane.getWidth() || x < 0){
+            System.out.println("You are out of borders, you cannot move outside of " + drawingPane.getWidth());
+            return 0;
+        }
+        if( y > drawingPane.getHeight()  || y < 0){
+            System.out.println("You are out of borders, you cannot move outside of" + drawingPane.getHeight());
+            return 0;
+        }
+        c.x = x;                               // New coordinates
+        c.y = y;
+        gc.moveTo(x, y);
+        System.out.println("POS changed");
+        return 1;
+    }
+
+    public int PRESS(Cursor c, double value){
+        if(value < 0 || value > 1){
+            System.out.println("You must put a number between 0 and 1");
+            return 0;
+        }
+        gc.setGlobalAlpha(value);
+        return 1;
+    }
+
+    public int LOOKAT(Cursor c, double x, double y, int percent) {
+        if(percent != 0 && percent != 1){
+            System.out.println("You must put a number between 0 and 1");
+            return 0;
+        }
+        if(percent == 1){
+            x = drawingPane.getPrefWidth() * x;
+            y = drawingPane.getPrefHeight() * y;
+        }
+        if(c.x == x && c.y == y){
+            c.rotation = 0;
+            return 1;
+        }
+        double hyp = Math.sqrt((x - c.x)*(x - c.x) + (y - c.y)*(y - c.y));
+        double opp = Math.sqrt((x - c.x)*(x - c.x));
+         c.rotation = Math.toDegrees(Math.acos(opp/hyp));
+        return 1;
+    }
+
+    public int LOOKATCursor(Cursor c1, Cursor c2){
+        return LOOKAT(c1, c2.x, c2.y, 0);
+    }
+
+    public int THICK(double value){
+        if(value < 0){
+            return  0;
+        }
+        gc.setLineWidth(value);// Width definition
+        return 1;
+    }
+
     // Method to interpret and execute drawing instruction
     private void interpretInstruction(String instruction) {
         String[] parts = instruction.split(" ");          // Create an array shares to store and use a space as a delimiter
@@ -324,75 +426,69 @@ public class InterfaceController {
             switch (command) {                              // Select the block of code to be executed
                 case "FWD":                                 // Moves the cursor relatively
                     if (parts.length < 2)
-                        throw new IllegalArgumentException("FWD command requires 1 parameter");           // Checks whether FWD is followed by a parameter
+                        throw new IllegalArgumentException("FWD command requires 1 parameter");
+                    System.out.println("Forward "+parts[1]);
                     FWD(cursor1, Double.parseDouble(parts[1]), 0);
                     break;
 
                 case "TURN":                    // rotates the cursor relatively in degrees
                     if (parts.length < 2) throw new IllegalArgumentException("TURN command requires 1 parameter");
+                    System.out.println("TURN "+parts[1]);
                     TURN(cursor1, Double.parseDouble(parts[1]));      // Adds the angle of rotation to the current one
                     break;
 
                 case "COLOR":                           // Determines the colour
                     if (parts.length < 2) throw new IllegalArgumentException("COLOR command requires 1 parameter");
-                    Color color = Color.web(parts[1]);  // Take the second element as a hexadecimal code or colour name
-                    gc.setStroke(color);
+                    System.out.println("COLOR "+parts[1]);
+                    COLORHEX(parts[1]);
                     break;
 
                 case "BWD":                     // Moves the cursor back relatively
                     if (parts.length < 2) throw new IllegalArgumentException("BWD command requires 1 parameter");
+                    System.out.println("BWD "+parts[1]);
                     BWD(cursor1,Double.parseDouble(parts[1]), 0);
                     break;
 
                 case "MOV":                     // Moves the cursor relatively
                     if (parts.length < 3) throw new IllegalArgumentException("MOV command requires 2 parameters");
-                    double movX = Double.parseDouble(parts[1]);
-                    double movY = Double.parseDouble(parts[2]);
-                    cursor1.x += movX;                  // Add mov
-                    cursor1.y += movY;
+                    System.out.println("MOV "+parts[1]+" "+parts[2]);
+                    MOV(cursor1, Double.parseDouble(parts[1]), Double.parseDouble(parts[2]), 0);
                     break;
 
                 case "THICK":                   // Is used to define the thickness of a line before moving the cursor
                     if (parts.length < 2) throw new IllegalArgumentException("THICK command requires 1 parameter");
-                    double thickness = Double.parseDouble(parts[1]);
-                    gc.setLineWidth(thickness);     // Width definition
+                    System.out.println("THICK "+parts[1]);
+                    THICK(Double.parseDouble(parts[1]));
                     break;
 
-//                case "HIDE":                    //hide the cursor on the screen
-//                    if (scene != null) {
-//                        scene.setCursor(Cursor.NONE);
-//                    }
-//                    break;
-//
-//                case "SHOW":                    //displays the cursor on the screen
-//                    if (scene != null) {
-//                        scene.setCursor(Cursor.DEFAULT);
-//                    }
-//                    break;
+                case "HIDE":                    //hide the cursor on the screen
+                    if (scene != null) {
+                        scene.setCursor(javafx.scene.Cursor.NONE);
+                    }
+                    break;
+
+                case "SHOW":                    //displays the cursor on the screen
+                    if (scene != null) {
+                        scene.setCursor(javafx.scene.Cursor.CROSSHAIR);
+                    }
+                    break;
 
                 case "POS":             // positions the cursor on the screen
                     if (parts.length < 3) throw new IllegalArgumentException("POS command requires 2 parameters");
-                    double posX = Double.parseDouble(parts[1]);
-                    double posY = Double.parseDouble(parts[2]);
-                    gc.moveTo(posX, posY);
-                    cursor1.x = posX;
-                    cursor1.y = posY;
+                    System.out.println("POS "+parts[1]+" "+parts[2]);
+                    POS(cursor1, Double.parseDouble(parts[1]), Double.parseDouble(parts[2]), 1);
                     break;
 
                 case "PRESS":           // indicates the pressure with which the cursor draws the shape
                     if (parts.length < 2) throw new IllegalArgumentException("PRESS command requires 1 parameter");
-                    double pressure;
-                    if (parts[1].endsWith("%")) {
-                        pressure = Double.parseDouble(parts[1].substring(0, parts[1].length() - 1)) / 100.0;
-                    } else {
-                        pressure = Double.parseDouble(parts[1]);
-                    }
-                    pressure = Math.min(Math.max(pressure, 0.0), 1.0);
-                    gc.setGlobalAlpha(pressure);
+                    System.out.println("PRESS "+parts[1]);
+                    PRESS(cursor1, Double.parseDouble(parts[1]));
                     break;
 
                 case "LOOKAT":          // rotates the current cursor
-                    // A implementer
+                    if (parts.length < 3) throw new IllegalArgumentException("LOOKAT command requires 1 parameter");
+                    System.out.println("LOOKAT "+parts[1]+" "+parts[2]);
+                    LOOKAT(cursor1,Double.parseDouble(parts[1]), Double.parseDouble(parts[2]), 1);
                     break;
 
                 default:
